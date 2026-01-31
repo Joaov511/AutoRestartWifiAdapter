308d94f591555fef6cebfdb66e43a3e4ff101731
João Vitor Von Ah <jv232479@gmail.com>
2026-01-31 10:38:45 -0300

    update date variable to get the correct time

--- restartIntertAdapter.ps1
+++ restartIntertAdapter.ps1
@@ -2,12 +2,12 @@
 [console]::WindowHeight=20; 
 [console]::BufferWidth=[console]::WindowWidth
 
-$currentTime = Get-Date -Format "HH:mm:ss"
 Write-Host "Internet Connection Monitor" -ForegroundColor Cyan
 Write-Host ""
 while ($true) {
     $pingResult = Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet
     if (-not $pingResult) {
+        $currentTime = Get-Date -Format "HH:mm:ss"
         Write-Host "Connection lost - Restarting Wi-Fi adapter... $($currentTime)" -ForegroundColor Red
         Restart-NetAdapter -Name "Wi-Fi" -Confirm:$false
         Start-Sleep -Seconds 10
