cf9650a2a85fd5239fca3f136982c570ef182b20
João Vitor Von Ah <jv232479@gmail.com>
2026-01-31 10:16:28 -0300

    add use relative path for batch file to run

--- rwifi.bat
+++ rwifi.bat
@@ -1,3 +1,2 @@
 @echo off
-mode con: cols=30 lines=10
-powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy RemoteSigned -File C:\Scripts\PersonalScripts\AutoRestartWifiAdapter\restartIntertAdapter.ps1' -Verb RunAs"
+powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy RemoteSigned -File %~dp0\restartIntertAdapter.ps1' -Verb RunAs"
